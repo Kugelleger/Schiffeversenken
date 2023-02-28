4198941fc932e9c93f2edda3f84cbf8dabbba543
Kugelleger <bjarne.ritzmann-moeller@gymhum.de>
2023-02-28 08:25:40 +0100

    d

---
+++ src/main/java/gymhum/de/Schiffe.java
@@ -0,0 +1,20 @@
+package gymhum.de;
+
+public class Schiffe {
+    int laenge;
+    
+    public Schiffe(int laenge){
+        setLaenge(laenge);
+    }
+
+
+    public void setLaenge(int laenge) {
+        this.laenge = laenge;
+    }
+    public int getLaenge() {
+        return laenge;
+    }
+
+
+
+}
